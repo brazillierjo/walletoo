c80acce81a885782d8da461a9ca2670f243b1086
brazillierjo <j.brazillier@gmail.com>
2023-11-12 12:43:54 +0100

    implement hero section on /

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "@/components/Header/Header";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,23 @@ interface Props {
 export default function RootLayout({ children }: Props) {
     return (
         <html lang='fr' suppressHydrationWarning>
-            <body className={inter.className}>
+            <body className={cn(inter.className, "relative")}>
                 <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
                     <Header />
                     {children}
                 </ThemeProvider>
+
+                <div
+                    className='absolute inset-x-0 top-0 -z-10 transform-gpu overflow-hidden blur-3xl'
+                    aria-hidden='true'>
+                    <div className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'></div>
+                </div>
+
+                <div
+                    className='absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]'
+                    aria-hidden='true'>
+                    <div className='relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]'></div>
+                </div>
             </body>
         </html>
     );
--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import { Route } from "@/utils/routes";
+import Logo from "../Common/Logo";
 
 function Hero() {
-  return <div>Hero</div>;
+    return (
+        <div className='px-6 h-fit mt-20 md:mt-0 md:h-screen flex flex-col justify-center lg:px-8'>
+            <div className='mx-auto flex flex-col gap-6 max-w-2xl'>
+                <div className='mb-8 mx-auto text-center w-fit justify-center'>
+                    <h1 className='rounded-full px-3 dark:text-gray-200 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-300/50'>
+                        Gérez vos finances en toute simplicité 🍀
+                    </h1>
+                </div>
+
+                <div className='text-center'>
+                    <h1 className='text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl'>
+                        Enrichissez votre expérience financière
+                    </h1>
+                    <p className='mt-6 text-lg leading-8 text-gray-600 dark:text-gray-200'>
+                        Waletoo vous aide à visualiser et optimiser vos finances personnelles avec des graphiques et des
+                        calculs intuitifs.
+                    </p>
+                    <div className='mt-10 flex items-center justify-center gap-x-6'>
+                        <Button>
+                            <Link href={Route.SIGNIN}>Je me lance ! 😍</Link>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Hero;
