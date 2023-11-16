9a1b9af17c6b9b9fc434e4c38658324dfcae6b70
brazillierjo <j.brazillier@gmail.com>
2023-11-16 20:08:21 +0100

    style links when active sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,14 +1,17 @@
 "use client";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 import { Separator } from "@/src/components/ui/separator";
 import { MdHome } from "react-icons/md";
 import { RiAccountPinCircleFill } from "react-icons/ri";
 import { IoIosSettings } from "react-icons/io";
 import { FaWallet } from "react-icons/fa6";
+import { cn } from "@/src/tools/tailwindMerge";
 
 const Sidebar: React.FC = () => {
     const { data: session } = useSession();
+    const pathname = usePathname();
 
     const links = [
         {
@@ -33,17 +36,26 @@ const Sidebar: React.FC = () => {
         },
     ];
 
+    const isActivelink = (path: string) => {
+        return pathname === path;
+    };
+
     return (
         <div className='dark:bg-primary-light bg-primary-light min-h-screen w-3/12'>
             <h2 className='px-4 py-8 text-center text-xl font-bold'>{session?.user?.name ?? "..."}</h2>
 
-            <Separator className='bg-gray-300 dark:bg-white' />
+            <Separator className='bg-gray-300' />
 
             <div className='flex flex-col gap-6 py-8 pl-8'>
                 {links.map((link, index) => (
                     <Link href={link.path} key={index} className='flex gap-3'>
-                        <link.icon className='h-6 w-6 fill-indigo-500 dark:fill-white' />
-                        <span>{link.name}</span>
+                        <link.icon
+                            className={cn(
+                                "h-6 w-6 fill-indigo-500 dark:fill-white",
+                                !isActivelink(link.path) && "opacity-40"
+                            )}
+                        />
+                        <span className={cn(!isActivelink(link.path) && "text-gray-400")}>{link.name}</span>
                     </Link>
                 ))}
             </div>
