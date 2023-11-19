eadebb1579c6af6bd8105bb4b75fa7116793979c
brazillierjo <j.brazillier@gmail.com>
2023-11-19 19:39:39 +0100

    fix google avatar

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
                 hostname: "avatars.githubusercontent.com",
                 pathname: "**",
             },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                pathname: "**",
+            },
         ],
     },
     experimental: {
