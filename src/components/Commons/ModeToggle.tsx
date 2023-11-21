498a6c968a1d63007296fde4027802f5480d5484
brazillierjo <j.brazillier@gmail.com>
2023-11-21 14:12:45 +0100

    add switch dark mode

--- src/components/Commons/ModeToggle.tsx
+++ src/components/Commons/ModeToggle.tsx
@@ -1,39 +1,21 @@
 "use client";
-
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import { Button } from "@/src/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/src/components/ui/dropdown-menu";
+import { Switch } from "@/src/components/ui/switch";
+import { Label } from "@/src/components/ui/label";
+import { MdOutlineDarkMode } from "react-icons/md";
 
 export function ModeToggle() {
-    const { setTheme } = useTheme();
+    const { theme, setTheme } = useTheme();
+
+    const handleMode = () => {
+        console.log("chenage");
+        theme === "dark" ? setTheme("light") : setTheme("dark");
+    };
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant='ghost' size='icon'>
-                    <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-                    <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-                    <span className='sr-only'>Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent align='end'>
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    Clair
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    Sombre
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    Système
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div className='flex items-center space-x-2'>
+            <Switch onClick={handleMode} />
+            <MdOutlineDarkMode />
+        </div>
     );
 }
