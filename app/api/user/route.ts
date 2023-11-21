d02a93d55eed607fcc4a5b9403fdade6544d23bf
brazillierjo <j.brazillier@gmail.com>
2023-11-21 10:08:47 +0100

    add put route to user api

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,6 +3,7 @@ import UserModel from "@/src/mongoDB/userSchema";
 import { authOptions } from "@/src/utils/authOptions";
 
 export const dynamic = "force-dynamic";
+
 export async function GET() {
     try {
         const session = await getServerSession(authOptions);
@@ -11,6 +12,25 @@ export async function GET() {
 
         const userEmail = session.user.email;
         const userInformations = await UserModel.find({ email: userEmail });
+
+        return new Response(JSON.stringify(userInformations));
+    } catch (error) {
+        console.error(error);
+        throw new Error("Internal Server Error");
+    }
+}
+
+export async function PUT(request: Request) {
+    try {
+        const session = await getServerSession(authOptions);
+
+        if (!session?.user?.email) throw new Error("Unauthorized");
+
+        const userEmail = session.user.email;
+        const userInformations = await UserModel.updateOne(
+            { email: userEmail },
+            { $set: await request.json() }
+        );
 
         return new Response(JSON.stringify(userInformations));
     } catch (error) {
