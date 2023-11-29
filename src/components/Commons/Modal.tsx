aeb103619110658e3e145ba682bbf2a10a06e501
brazillierjo <j.brazillier@gmail.com>
2023-11-29 14:56:31 +0100

    prettier new files

--- src/components/Commons/Modal.tsx
+++ src/components/Commons/Modal.tsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
+import { useState } from "react"
+import { Dialog } from "@headlessui/react"
 
 type ModalProps = {
-  title: string;
-  description: string;
-  children: React.ReactNode;
-};
+  title: string
+  description: string
+  children: React.ReactNode
+}
 
 export const Modal: React.FC<ModalProps> = ({ title, description, children }) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(true)
 
   return (
     <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
       <Dialog.Panel>
         <Dialog.Title>{title}</Dialog.Title>
         <Dialog.Description>{description}</Dialog.Description>
-
         {children}
       </Dialog.Panel>
     </Dialog>
-  );
-};
+  )
+}
--- src/components/Wallet/TransactionTable.tsx
+++ src/components/Wallet/TransactionTable.tsx
@@ -1,135 +1,145 @@
-"use client";
+"use client"
 
-import { useMemo, useState } from "react";
-import { userAtom } from "@/src/atoms/user.atom";
-import FormattedTransaction from "@/src/components/Commons/FormattedTransaction";
-import { Card } from "@/src/components/ui/card";
-import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/src/components/ui/table";
-import { CreateTransactionForm } from "@/src/components/Wallet/CreateTransactionForm";
-import { TransactionFilter } from "@/src/enums/transactionFilter";
-import { TransactionType } from "@/src/enums/transactionType";
-import { makeCardOpacity } from "@/src/utils/animations";
-import { cn } from "@/src/utils/tailwindMerge";
-import { motion } from "framer-motion";
-import { useAtom } from "jotai";
-import { IoChevronDownOutline } from "react-icons/io5";
+import { useMemo, useState } from "react"
+import { userAtom } from "@/src/atoms/user.atom"
+import FormattedTransaction from "@/src/components/Commons/FormattedTransaction"
+import { Modal } from "@/src/components/Commons/Modal"
+import { Card } from "@/src/components/ui/card"
+import { Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/src/components/ui/table"
+import { CreateTransactionForm } from "@/src/components/Wallet/CreateTransactionForm"
+import { TransactionFilter } from "@/src/enums/transactionFilter"
+import { TransactionType } from "@/src/enums/transactionType"
+import { makeCardOpacity } from "@/src/utils/animations"
+import { cn } from "@/src/utils/tailwindMerge"
+import { motion } from "framer-motion"
+import { useAtom } from "jotai"
+import { IoChevronDownOutline } from "react-icons/io5"
 
 type TransactionTableProps = {
-  type: TransactionType;
-};
+  type: TransactionType
+}
 
 export const TransactionTable: React.FC<TransactionTableProps> = ({ type }) => {
-  const [user, setUser] = useAtom(userAtom);
-  const [sortType, setSortType] = useState<TransactionFilter>(TransactionFilter.AmountDESC);
+  const [user, setUser] = useAtom(userAtom)
+  const [sortType, setSortType] = useState<TransactionFilter>(TransactionFilter.AmountDESC)
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false)
 
-  const transactions = type === TransactionType.INCOMES ? user?.incomes : user?.expenses;
+  const transactions = type === TransactionType.INCOMES ? user?.incomes : user?.expenses
 
   const total = useMemo(() => {
-    if (!transactions) return 0;
+    if (!transactions) return 0
 
-    return transactions.reduce((acc, curr) => acc + curr.amount, 0);
-  }, [transactions]);
+    return transactions.reduce((acc, curr) => acc + curr.amount, 0)
+  }, [transactions])
 
   const sortedTransactions = useMemo(() => {
-    if (!transactions) return [];
+    if (!transactions) return []
 
     return [...transactions].sort((a, b) => {
       switch (sortType) {
         case TransactionFilter.LabelDESC:
-          return a.label.localeCompare(b.label);
+          return a.label.localeCompare(b.label)
         case TransactionFilter.LabelASC:
-          return b.label.localeCompare(a.label);
+          return b.label.localeCompare(a.label)
         case TransactionFilter.AmountASC:
-          return a.amount - b.amount;
+          return a.amount - b.amount
         case TransactionFilter.AmountDESC:
         default:
-          return b.amount - a.amount;
+          return b.amount - a.amount
       }
-    });
-  }, [transactions, sortType]);
+    })
+  }, [transactions, sortType])
 
   const toggleLabelSort = () => {
     setSortType((prevType) =>
       prevType === TransactionFilter.LabelDESC ? TransactionFilter.LabelASC : TransactionFilter.LabelDESC
-    );
-  };
+    )
+  }
 
   const toggleAmountSort = () => {
     setSortType((prevType) =>
       prevType === TransactionFilter.AmountDESC ? TransactionFilter.AmountASC : TransactionFilter.AmountDESC
-    );
-  };
+    )
+  }
 
-  if (!transactions || !user) return null;
+  if (!transactions || !user) return null
 
   return (
-    <motion.div
-      className="w-full"
-      initial="hidden"
-      animate="visible"
-      variants={type === TransactionType.EXPENSES ? makeCardOpacity(0.2) : makeCardOpacity()}
-    >
-      <Card className="flex h-full flex-col rounded-md p-4">
-        <h2 className="mb-4 text-lg font-semibold">{type}</h2>
+    <>
+      <motion.div
+        className="w-full"
+        initial="hidden"
+        animate="visible"
+        variants={type === TransactionType.EXPENSES ? makeCardOpacity(0.2) : makeCardOpacity()}
+      >
+        <Card className="flex h-full flex-col rounded-md p-4">
+          <h2 className="mb-4 text-lg font-semibold">{type}</h2>
 
-        <div className="mb-4 px-2">
-          {sortedTransactions.length > 0 ? (
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>
-                    Label
-                    <button onClick={toggleLabelSort}>
-                      <IoChevronDownOutline
-                        className={cn(
-                          "ml-1 h-3 w-3",
-                          sortType === TransactionFilter.LabelASC ? "rotate-180" : "rotate-0"
-                        )}
-                      />
-                    </button>
-                  </TableHead>
-                  <TableHead className="pb-1 text-right text-sm uppercase">
-                    Montant
-                    <button onClick={toggleAmountSort}>
-                      <IoChevronDownOutline
-                        className={cn(
-                          "ml-1 h-3 w-3",
-                          sortType === TransactionFilter.AmountASC ? "rotate-180" : "rotate-0"
-                        )}
-                      />
-                    </button>
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
+          <div className="mb-4 px-2">
+            {sortedTransactions.length > 0 ? (
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>
+                      Label
+                      <button onClick={toggleLabelSort}>
+                        <IoChevronDownOutline
+                          className={cn(
+                            "ml-1 h-3 w-3",
+                            sortType === TransactionFilter.LabelASC ? "rotate-180" : "rotate-0"
+                          )}
+                        />
+                      </button>
+                    </TableHead>
+                    <TableHead className="pb-1 text-right text-sm uppercase">
+                      Montant
+                      <button onClick={toggleAmountSort}>
+                        <IoChevronDownOutline
+                          className={cn(
+                            "ml-1 h-3 w-3",
+                            sortType === TransactionFilter.AmountASC ? "rotate-180" : "rotate-0"
+                          )}
+                        />
+                      </button>
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
 
-              <TableBody>
-                {sortedTransactions.map((transaction, index) => (
-                  <TableRow key={index} className="cursor-pointer" onClick={() => console.log("click")}>
-                    <TableCell>{transaction.label}</TableCell>
+                <TableBody>
+                  {sortedTransactions.map((transaction, index) => (
+                    <TableRow key={index} className="cursor-pointer" onClick={() => setIsTransactionModalOpen(true)}>
+                      <TableCell>{transaction.label}</TableCell>
 
+                      <TableCell className="text-right">
+                        <FormattedTransaction amount={transaction.amount} />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+
+                <TableFooter>
+                  <TableRow>
+                    <TableCell colSpan={1}>Total</TableCell>
                     <TableCell className="text-right">
-                      <FormattedTransaction amount={transaction.amount} />
+                      <FormattedTransaction amount={total} />
                     </TableCell>
                   </TableRow>
-                ))}
-              </TableBody>
+                </TableFooter>
+              </Table>
+            ) : (
+              <p className="mt-8 text-center text-xs italic">Aucune transaction enregistrée.</p>
+            )}
+          </div>
 
-              <TableFooter>
-                <TableRow>
-                  <TableCell colSpan={1}>Total</TableCell>
-                  <TableCell className="text-right">
-                    <FormattedTransaction amount={total} />
-                  </TableCell>
-                </TableRow>
-              </TableFooter>
-            </Table>
-          ) : (
-            <p className="mt-8 text-center text-xs italic">Aucune transaction enregistrée.</p>
-          )}
-        </div>
+          <CreateTransactionForm type={type} user={user} setUser={setUser} />
+        </Card>
+      </motion.div>
 
-        <CreateTransactionForm type={type} user={user} setUser={setUser} />
-      </Card>
-    </motion.div>
-  );
-};
+      {isTransactionModalOpen && (
+        <Modal title="Ajouter une transaction" description="Ajouter une transaction à votre portefeuille.">
+          <p>test</p>
+        </Modal>
+      )}
+    </>
+  )
+}
