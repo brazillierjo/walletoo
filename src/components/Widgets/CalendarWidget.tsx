7a32bc565f0301fed199ec87b3e4dc14d9083ca3
brazillierjo <j.brazillier@gmail.com>
2023-12-04 16:55:03 +0100

    implement locale

--- src/components/Widgets/CalendarWidget.tsx
+++ src/components/Widgets/CalendarWidget.tsx
@@ -5,7 +5,6 @@ import { userAtom } from "@/src/atoms/user.atom";
 import { Calendar } from "@/src/components/ui/calendar";
 import { Card } from "@/src/components/ui/card";
 import { getLocale } from "@/src/utils/locales";
-import { enUS, fr } from "date-fns/locale";
 import { useAtom } from "jotai";
 
 export const CalendarWidget: React.FC = () => {
@@ -16,7 +15,7 @@ export const CalendarWidget: React.FC = () => {
 
   return (
     <Card className="w-fit">
-      <Calendar mode="single" selected={date} onSelect={setDate} locale={fr} className="rounded-md border" />
+      <Calendar mode="single" selected={date} onSelect={setDate} locale={locale} className="rounded-md border" />
     </Card>
   );
 };
