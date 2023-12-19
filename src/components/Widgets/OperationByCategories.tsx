554225914b0c32bd4b31d9ec1ab669fc06238b16
brazillierjo <j.brazillier@gmail.com>
2023-12-19 16:42:56 +0100

    Update max height of tabs content

--- src/components/Widgets/OperationByCategories.tsx
+++ src/components/Widgets/OperationByCategories.tsx
@@ -37,7 +37,7 @@ export const OperationByCategories = () => {
             <TabsTrigger value={OperationType.EXPENSES}>{OperationTypeLabel.EXPENSES}</TabsTrigger>
           </TabsList>
 
-          <TabsContent className="flex max-h-[400px] flex-col gap-4 overflow-y-auto" value={OperationType.INCOMES}>
+          <TabsContent className="flex max-h-[300px] flex-col gap-4 overflow-y-auto" value={OperationType.INCOMES}>
             {uniqueUserIncomesCategories.map((category) => (
               <Card key={category} className="w-full border-0 p-1 shadow-none">
                 <h4 className="mb-3 font-semibold">{category ?? "Non catégorisés"}</h4>
@@ -63,7 +63,7 @@ export const OperationByCategories = () => {
             ))}
           </TabsContent>
 
-          <TabsContent className="flex max-h-[400px] flex-col gap-4 overflow-y-auto" value={OperationType.EXPENSES}>
+          <TabsContent className="flex max-h-[300px] flex-col gap-4 overflow-y-auto" value={OperationType.EXPENSES}>
             {uniqueUserExpensesCategories.map((category) => (
               <Card key={category} className="w-full border-0 p-1 shadow-none">
                 <h4 className="mb-3 font-semibold">{category ?? "Non catégorisés"}</h4>
