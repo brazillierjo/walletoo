9776fad24a4864f5d98a23415129611be37015f0
brazillierjo <j.brazillier@gmail.com>
2023-12-29 00:42:47 +0100

    Refactor table component styles

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -24,7 +24,7 @@ TableBody.displayName = "TableBody";
 
 const TableFooter = React.forwardRef<HTMLTableSectionElement, React.HTMLAttributes<HTMLTableSectionElement>>(
   ({ className, ...props }, ref) => (
-    <tfoot ref={ref} className={cn("border-t bg-muted/50 font-medium [&>tr]:last:border-b-0", className)} {...props} />
+    <tfoot ref={ref} className={cn("bg-muted/50 border-t font-medium [&>tr]:last:border-b-0", className)} {...props} />
   )
 );
 TableFooter.displayName = "TableFooter";
@@ -33,7 +33,10 @@ const TableRow = React.forwardRef<HTMLTableRowElement, React.HTMLAttributes<HTML
   ({ className, ...props }, ref) => (
     <tr
       ref={ref}
-      className={cn("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
+      className={cn(
+        "hover:bg-muted/50 border-b transition-colors data-[state=selected]:bg-muted dark:border-slate-500",
+        className
+      )}
       {...props}
     />
   )
