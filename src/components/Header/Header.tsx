7d3078fa27fa09296ee6ed29a5491d2499d68583
brazillierjo <j.brazillier@gmail.com>
2024-01-02 15:14:34 +0100

    Add @radix-ui/react-dialog dependency and update Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,71 +6,66 @@ import { LogButton } from "@/src/components/Commons/LogButton";
 import { Logo } from "@/src/components/Commons/Logo";
 import { ModeToggle } from "@/src/components/Commons/ModeToggle";
 import { Button } from "@/src/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/src/components/ui/dropdown-menu";
-import { IUser } from "@/src/interfaces/userInterface";
-import { links, RouterLinkType } from "@/src/utils/links";
-import { useWindowSize } from "@uidotdev/usehooks";
+import { NavigationMenu, NavigationMenuLink, NavigationMenuList } from "@/src/components/ui/navigation-menu";
+import { Sheet, SheetContent, SheetTrigger } from "@/src/components/ui/sheet";
+import { links } from "@/src/utils/links";
 import { useAtom } from "jotai";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 export const Header: React.FC = () => {
   const [user] = useAtom(userAtom);
-  if (!user) return null;
 
-  const { width } = useWindowSize();
   const navigationLinks = links.filter((link) => link.isInHeader || link.isInSidebar);
-  const isMobile = width && width < 1280;
 
-  const headerContent = !isMobile ? (
-    <>
-      <div className="flex items-center gap-2 text-xl font-bold">
-        <Logo withLabel withCatchPhrase />
+  return (
+    <header className="flex h-14 w-full items-center justify-between px-4 md:px-6">
+      <Logo withLabel withCatchPhrase />
+
+      <div className="hidden lg:block">
+        <NavigationMenu>
+          <NavigationMenuList className="flex items-center gap-4">
+            {navigationLinks.map((link, index) => (
+              <NavigationMenuLink key={index} asChild>
+                {link.isSubscribedRequired && !user?.isSubscribed ? (
+                  <DisabledLink key={index} link={link} />
+                ) : (
+                  <RouterLink key={index} link={link} />
+                )}
+              </NavigationMenuLink>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
       </div>
 
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button className="lg:hidden" size="icon" variant="outline">
+              <RxHamburgerMenu className="h-4 w-4" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side="right">
+            <div className="grid gap-2 py-6">
+              {navigationLinks.map((link, index) =>
+                link.isSubscribedRequired && !user?.isSubscribed ? (
+                  <DisabledLink key={index} className={{ container: "my-2" }} link={link} withIcon />
+                ) : (
+                  <RouterLink key={index} className={{ container: "my-2" }} link={link} withIcon />
+                )
+              )}
+
+              <div className="mt-5">
+                <LogButton withIcon />
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
+
         <ModeToggle />
-        <BurgerMenu user={user} navigationLinks={navigationLinks} />
         <LogButton withIcon />
       </div>
-    </>
-  ) : (
-    <BurgerMenu user={user} navigationLinks={navigationLinks} />
-  );
-
-  return (
-    <header>
-      <nav className="mx-auto flex items-center justify-between px-4 py-2 lg:px-8">{headerContent}</nav>
     </header>
-  );
-};
-
-interface BurgerMenuProps {
-  user: IUser;
-  navigationLinks: RouterLinkType[];
-}
-
-export const BurgerMenu: React.FC<BurgerMenuProps> = ({ user, navigationLinks }) => {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Button variant="outline">
-          <RxHamburgerMenu className="h-4 w-4" />
-        </Button>
-      </DropdownMenuTrigger>
-
-      <DropdownMenuContent className="min-w-[160px] px-4 py-2">
-        {navigationLinks.map((link, index) =>
-          link.isSubscribedRequired && !user?.isSubscribed ? (
-            <DisabledLink key={index} className={{ container: "my-2" }} link={link} />
-          ) : (
-            <RouterLink key={index} className={{ container: "my-2" }} link={link} />
-          )
-        )}
-
-        <div className="mt-4">
-          <LogButton />
-        </div>
-      </DropdownMenuContent>
-    </DropdownMenu>
   );
 };
