5ac8be60cb5223d8f0ad3a90b217b99b23cc23f0
brazillierjo <j.brazillier@gmail.com>
2024-01-08 08:32:50 +0100

    Remove unused dependencies and charts

--- src/app/(protected-routes)/wallet/page.tsx
+++ src/app/(protected-routes)/wallet/page.tsx
@@ -1,30 +1,20 @@
 "use client";
 
-import { useState } from "react";
 import { selectedOperationPanelAtom } from "@/src/atoms/panel.atom";
 import { selectedOperationAtom } from "@/src/atoms/selectedOperation.atom";
 import { userAtom } from "@/src/atoms/user.atom";
-import PieChart from "@/src/components/Charts/PieChart";
 import Panel from "@/src/components/Commons/Panel";
-import { Button } from "@/src/components/ui/button";
 import BalanceTable from "@/src/components/Wallet/BalanceTable";
 import { EditOperationForm } from "@/src/components/Wallet/EditOperationForm";
 import { OperationByCategories } from "@/src/components/Wallet/OperationByCategories";
 import { OperationTable } from "@/src/components/Wallet/OperationTable";
 import { OperationTypeLabel } from "@/src/enums/operationType";
 import { useAtom } from "jotai";
-import { FaChartSimple } from "react-icons/fa6";
 
 const Wallet: React.FC = () => {
-  const [areChartsDisplay, setAreChartsDisplay] = useState(false);
   const [user] = useAtom(userAtom);
   const [showPanel, setShowPanel] = useAtom(selectedOperationPanelAtom);
   const [selectedOperation] = useAtom(selectedOperationAtom);
-
-  const incomesLabels = user?.incomes.map((operation) => operation.label);
-  const incomesData = user?.incomes.map((operation) => operation.amount);
-  const expensesLabels = user?.expenses.map((operation) => operation.label);
-  const expensesData = user?.expenses.map((operation) => operation.amount);
 
   if (!user) return null;
 
@@ -45,25 +35,6 @@ const Wallet: React.FC = () => {
           </div>
         )}
       </div>
-
-      {areChartsDisplay ? (
-        <div className="flex flex-col justify-between gap-5 md:flex-row">
-          <div className="w-full md:w-1/2">
-            <PieChart title="Répartition des revenus" labels={incomesLabels ?? []} data={incomesData ?? []} />
-          </div>
-          <div className="w-full md:w-1/2">
-            <PieChart title="Répartition des dépenses" labels={expensesLabels ?? []} data={expensesData ?? []} />
-          </div>
-        </div>
-      ) : user.incomes.length > 0 && user.expenses.length > 0 ? (
-        <Button className="mx-auto flex w-fit items-center gap-2" onClick={() => setAreChartsDisplay(true)}>
-          Afficher les graphiques <FaChartSimple />
-        </Button>
-      ) : (
-        <small className="text-center italic opacity-80">
-          Ajouter des opérations pour voir les détails et analyses de celles-ci.
-        </small>
-      )}
 
       {showPanel && selectedOperation && (
         <Panel key={selectedOperation.operation._id} onClose={() => setShowPanel(false)}>
