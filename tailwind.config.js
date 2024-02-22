bdf32f2a88aa44fa796a1266bd3fa5168a8327c7
BRAZILLIER Johan <j.brazillier@gmail.com>
2024-02-22 21:29:46 +0100

    Remove unused linear background styles and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,6 @@ module.exports = {
       },
     },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
         border: "var(--border)",
         input: "var(--input)",
